fc4e04187379b26a5db8696b5c550eb2602fba0c
RajanKunwar <75790060+RajanKunwar@users.noreply.github.com>
2021-07-29 20:46:58 -0700

    Add files via upload

---
+++ project/admin/partials/footer.php
@@ -0,0 +1,10 @@
+	<!--footer section start -->
+<div class="footer">
+	<div class="wrapper">
+<p class="text-center">I had a hard time creating this project. So, for more information <a href="#">Click Here</a></p>
+    </div>         
+</div>
+	<!--footer section end -->
+
+</body>
+</html>
---
+++ project/admin/partials/menu.php
@@ -0,0 +1,23 @@
+
+<?php  include('../config/constants.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Food Order</title>
+	<link rel="stylesheet" type="text/css" href="../css/admin.css">
+</head>
+<body>
+	<!--menu select start --> 
+<div class="menu text-center">
+	<div class="wrapper">
+<ul>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="manage-admin.php">Admin</a></li>
+	<li><a href="manage-category.php">Category</a></li>
+	<li><a href="manage-food.php">Food</a></li>
+	<li><a href="manage-order.php">Order</a></li>
+</ul>
+	</div>
+</div>
+	<!--menue select end -->
